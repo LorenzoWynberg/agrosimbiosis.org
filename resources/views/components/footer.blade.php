12dadd88b0c0b5eb8bfc6efb667fe606907c241b
LorenzoWynberg <lwcincr93@gmail.com>
2021-12-07 14:54:44 -0600

    Footer improvements

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,90 +1,87 @@
-<footer class="text-center text-lg-start bg-success bg-opacity-50 text-white">
-  <!-- Grid container -->
-  <div class="container p-4">
-    <!--Grid row-->
-    <div class="row mt-4">
-      <!--Grid column-->
-      <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase mb-4">Acerca de Nosotros</h5>
+<footer class="text-center text-lg-start text-white">
+  <div class="bg-success bg-opacity-50">  
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row mt-4">
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+          <h5 class="text-uppercase mb-4">Acerca de Nosotros</h5>
 
-        <p>
-          At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-          voluptatum deleniti atque corrupti.
-        </p>
+          <p>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+            voluptatum deleniti atque corrupti.
+          </p>
 
-        <p>
-          Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
-          molestias.
-        </p>
+          <p>
+            Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
+            molestias.
+          </p>
 
-        <div class="mt-4">
-          <!-- Facebook -->
-          <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-facebook-f"></i></a>
-          <!-- Dribbble -->
-          <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-dribbble"></i></a>
-          <!-- Twitter -->
-          <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-twitter"></i></a>
-          <!-- Google + -->
-          <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-google-plus-g"></i></a>
-          <!-- Linkedin -->
+          <div class="mt-4">
+            <!-- Facebook -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-facebook-f"></i></a>
+            <!-- Dribbble -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-dribbble"></i></a>
+            <!-- Twitter -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-twitter"></i></a>
+            <!-- Google + -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-google-plus-g"></i></a>
+            <!-- Linkedin -->
+          </div>
         </div>
-      </div>
-      <!--Grid column-->
+        <!--Grid column-->
 
-      <!--Grid column-->
-      <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase mb-4 pb-1">Busqueda</h5>
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4">Horario</h5>
 
-        <div class="form-outline form-white mb-4">
-          <input type="text" id="formControlLg" class="form-control form-control-lg" />
-          <label class="form-label" for="formControlLg">Busqueda dentro del sitio</label>
+          <table class="table text-center">
+            <tbody class="fw-normal text-white">
+              <tr>
+                <td>Lun - Vie:</td>
+                <td>7am - 3pm</td>
+              </tr>
+              <tr>
+                <td>Sab - Sab:</td>
+                <td>7am - 1am</td>
+              </tr>
+              <tr>
+                <td>Domingo:</td>
+                <td>Cerrado</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+        <!--Grid column-->
 
-        <ul class="fa-ul" style="margin-left: 1.65em;">
-          <li class="mb-3">
-            <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2">Belen, Heredia</span>
-          </li>
-          <li class="mb-3">
-            <span class="fa-li"><i class="fas fa-envelope"></i></span><span class="ms-2">agrosimbiosis@gmail.com</span>
-          </li>
-          <li class="mb-3">
-            <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2">+506 8329-3003</span>
-          </li>
-        </ul>
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4 pb-1">Contacto</h5>
+          <ul class="fa-ul mx-0">
+            <li class="mb-3">
+              <span class="fa-li position-static"><i class="fas fa-phone"></i></span><span class="ms-2">+506 8329-3003</span>
+            </li>
+            <li class="mb-3">
+              <span class="fa-li position-static"><i class="fas fa-envelope"></i></span><span class="ms-2">agrosimbiosis@gmail.com</span>
+            </li>
+            <li class="mb-3">
+              <span class="fa-li position-static"><i class="fas fa-home"></i></span><span class="ms-2">Belén, Heredia</span>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
       </div>
-      <!--Grid column-->
-
-      <!--Grid column-->
-      <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase mb-4">Horario</h5>
-
-        <table class="table text-center">
-          <tbody class="fw-normal text-white">
-            <tr>
-              <td>Lun - Vie:</td>
-              <td>7am - 3pm</td>
-            </tr>
-            <tr>
-              <td>Sab - Sab:</td>
-              <td>7am - 1am</td>
-            </tr>
-            <tr>
-              <td>Domingo:</td>
-              <td>Cerrado</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <!--Grid column-->
+      <!--Grid row-->
     </div>
-    <!--Grid row-->
+    <!-- Grid container -->
   </div>
-  <!-- Grid container -->
-
   <!-- Copyright -->
-  <div class="text-center p-3 bg-info bg-opacity-75 text-white">
-    © 2021 Copyright:
-    <a class="text-white" href="https://mdbootstrap.com/">AgroSimbiosis.org</a>
+  <div class="copyright">
+    <div class="text-center p-3 bg-primary bg-opacity-50 text-white">
+      © 2021 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">AgroSimbiosis.org</a>
+    </div>
   </div>
   <!-- Copyright -->
 </footer>
