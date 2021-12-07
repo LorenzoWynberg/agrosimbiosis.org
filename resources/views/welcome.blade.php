95b604d8f028fc3187cea700edd39c94cc3ce97e
LorenzoWynberg <lwcincr93@gmail.com>
2021-12-07 08:24:31 -0600

    Main nav setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Agrosimbiosis</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
         <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        
+        @include('components.main-nav')
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
