2c79974795dcd12014022708666918e4afc9c82a
LorenzoWynberg <lwcincr93@gmail.com>
2021-12-15 10:23:24 -0600

    Added some basic styling to main-nav

--- resources/views/components/main-nav.blade.php
+++ resources/views/components/main-nav.blade.php
@@ -1,10 +1,10 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light" id="main-nav">
-  <div class="container-fluid">
-    <a class="navbar-brand text-primary" href="#">Agrosimbiosis</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-success bg-opacity-75" id="main-nav">
+  <div class="container-fluid ">
+    <a class="navbar-brand" href="#">Agrosimbiosis</a>
+    <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse mt-1 mt-lg-0" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Inicio</a>
@@ -20,9 +20,12 @@
         </li>
       </ul>
       <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Busqueda" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Busqueda</button>
+        <input class="form-control outline-info me-2" type="search" placeholder="Busqueda" aria-label="Search">
+        <button class="btn btn-info text-white" type="submit">Busqueda</button>
       </form>
     </div>
+    <!-- .collapse END -->
   </div>
+  <!-- .container-fluid END -->
 </nav>
+<!-- .navbar END -->
