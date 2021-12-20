6c746065249cd15ff6950f24af0cc5fd2fe1ef89
LorenzoWynberg <lwcincr93@gmail.com>
2021-12-19 23:06:46 -0600

    Switched to 2 space tabs

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,20 +1,20 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}" xmlns="http://www.w3.org/1999/html">
-    <head>
-        @yield('meta')
-        <meta charset="utf-8">
-        <title>@yield('title') - Agrosimbiosis</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-    </head>
-    <body id="@yield('id')">
-        @include('components.main-nav')
-        <div id="wrapper">
-            @yield('content')
-        </div>
-        @include('components.footer')
-        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
-        <script src="https://kit.fontawesome.com/c432b96f6c.js" crossorigin="anonymous"></script>
-        @yield('scripts')
-    </body>
+  <head>
+    @yield('meta')
+    <meta charset="utf-8">
+    <title>@yield('title') - Agrosimbiosis</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+  </head>
+  <body id="@yield('id')">
+    @include('components.main-nav')
+    <div id="wrapper">
+      @yield('content')
+    </div>
+    @include('components.footer')
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <script src="https://kit.fontawesome.com/c432b96f6c.js" crossorigin="anonymous"></script>
+    @yield('scripts')
+  </body>
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,23 +4,23 @@
 @section('id', 'home')
 
 @section('content')
-	<div id="hero">
-	    <div class="overlay">
-	        <div class="container-fluid">
-	            <div class="row align-items-end pt-5 pb-5">
-	                <div class="col-9 mx-auto text-center">
-	                    <h1 class="text-white display-2">Agrosimbiosis</h1>
-	                    <h2 class="text-white fw-light">
-	                      Cosechando Ciclos
-	                    </h2>
-	                    <p>
-	                        <a id="hero-cta" href="#" class="btn btn-primary text-white text-uppercase py-2 px-4">
-	                            Conocenos
-	                        </a>
-	                    </p>
-	                </div>
-	            </div>
-	        </div>
-	    </div>
-	</div>
+<div id="hero">
+  <div class="overlay">
+    <div class="container-fluid">
+      <div class="row align-items-end pt-5 pb-5">
+        <div class="col-9 mx-auto text-center">
+          <h1 class="text-white display-2">Agrosimbiosis</h1>
+          <h2 class="text-white fw-light">
+            Cosechando Ciclos
+          </h2>
+          <p>
+            <a id="hero-cta" href="#" class="btn btn-primary text-white text-uppercase py-2 px-4">
+                Conocenos
+            </a>
+          </p>
+        </div>
+    	</div>
+    </div>
+  </div>
+</div>
 @stop
