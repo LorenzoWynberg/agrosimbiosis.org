d5d785f433898952d763e1b974991daa1a0a5aff
LorenzoWynberg <lwcincr93@gmail.com>
2021-12-19 22:05:59 -0600

    Initial hero

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,25 @@
     </head>
     <body>
         @include('components.main-nav')
-        <div style="height:2000px"></div>
+        <div id="hero">
+            <div class="overlay">
+                <div class="container-fluid">
+                    <div class="row align-items-end pt-5 pb-5">
+                        <div class="col-9 mx-auto text-center">
+                            <h1 class="text-white display-2">Agrosimbiosis</h1>
+                            <h2 class="text-white fw-light">
+                              Cosechando Ciclos
+                            </h2>
+                            <p>
+                                <a id="hero-cta" href="#" class="btn btn-primary text-white text-uppercase py-2 px-4">
+                                    Conocenos
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         @include('components.footer')
         <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
         <script src="https://kit.fontawesome.com/c432b96f6c.js" crossorigin="anonymous"></script>
