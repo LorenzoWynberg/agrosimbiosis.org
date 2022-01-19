28a1b60c0176a1591f44e2f52d444416ac7c5c73
LorenzoWynberg <lwcincr93@gmail.com>
2022-01-18 19:32:40 -0600

    Added #pillars section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -25,8 +25,34 @@
   </div>
 </div>
 
-<div id="mision">
+<div id="pillars">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-12 col-sm-6 col-lg-3 my-5">
+       <img src="{{ asset('imgs/icons/health.png') }}" width="200" alt="">
+       <h3 class="mt-2">Salud</h3>
+      </div>
+      <hr class="d-sm-none">
+      <div class="col-12 col-sm-6 col-lg-3 my-5">
+        <img src="{{ asset('imgs/icons/regeneration.png') }}" width="200" alt="">
+        <h3 class="mt-2">Regeneración</h3>
+      </div>
+      <hr class="d-lg-none">
+      <div class="col-12 col-sm-6 col-lg-3 my-5">
+        <img src="{{ asset('imgs/icons/symbiosis.png') }}" width="200" alt="">
+        <h3 class="mt-2">Symbiosis</h3>
+      </div>
+      <hr class="d-sm-none">
+      <div class="col-12 col-sm-6 col-lg-3 my-5">
+        <img src="{{ asset('imgs/icons/empathy.png') }}" width="200" alt="">
+        <h3 class="mt-2">Empatía</h3>
+      </div>
+    </div>
+  </div>
+</div>
 
+<div id="mision" class="py-5">
+  
 </div>
 
 @stop
