bd9dc06c7efc8ae4d3c55c5637af23504ab37db1
LorenzoWynberg <lwcincr93@gmail.com>
2022-04-05 11:19:24 -0600

    Changed redirect links for social providers in services to env variables

--- config/services.php
+++ config/services.php
@@ -34,19 +34,19 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' =>  '/auth/google/callback',
+        'redirect' =>  env('GOOGLE_CLIENT_CALLBACK'),
     ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' =>  '/auth/facebook/callback',
+        'redirect' =>  env('FACEBOOK_CLIENT_CALLBACK'),
     ],
 
-    'google' => [
-        'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' =>  '/auth/google/callback',
+    'twitter' => [
+        'client_id' => env('TWITTER_CLIENT_ID'),
+        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+        'redirect' =>  env('TWITTER_CLIENT_CALLBACK'),
     ],
 
 ];
